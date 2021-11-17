4696363ed2012b1f348b0d6b066d04decd1435f1
Isienyi Paschal Ejike <candlepeter56@gmail.com>
2021-11-17 13:40:24 +0100

    completed the Javascript solution for the challenge

--- index.js
+++ index.js
@@ -9,12 +9,54 @@ const header = document.querySelector("#header");
 count = 0;
 
 function prevSlide() {
-  
+  if (count === 0) {
+    //removes the previous classlist
+    slides[count].classList.remove("current");
+    shopSlides[count].classList.remove("current");
+    //initializes the count to the last slide
+    count = (slides.length-1);
+    //adds the last slide
+    slides[count].classList.add("current");
+    shopSlides[count].classList.add("current");
+  } else {
+    //removes the previous slide
+    slides[count].classList.remove("current");
+    shopSlides[count].classList.remove("current");
+    //decrements the count
+    count--;
+    //set in the next slide
+    slides[count].classList.add("current");
+    shopSlides[count].classList.add("current");
+  }
 }
 
 function nextSlide() {
-  
+  if (count === (slides.length-1)) {
+    //removes the previous classlist
+    slides[count].classList.remove("current");
+    shopSlides[count].classList.remove("current");
+    //initializes the count
+    count = 0;
+    //resets the slide
+    startSlide();
+  } else {
+    //removes the previous slide
+    slides[count].classList.remove("current");
+    shopSlides[count].classList.remove("current");
+    //increments the count
+    count++
+    //set in the next slide
+    slides[count].classList.add("current");
+    shopSlides[count].classList.add("current");
+  }
 }
+
+function startSlide() {
+  slides[0].classList.add("current");
+  shopSlides[0].classList.add("current");
+}
+
+startSlide();
 
 arrowLeft.addEventListener("click", prevSlide)
 
